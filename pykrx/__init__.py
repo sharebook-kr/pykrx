1d8ce17628233d54e7fdfd592831f6e781cbb40a
jonghun.yoo <jonghun.yoo@outlook.com>
2021-02-03 20:13:07 +0900

    DataFrame 시각화 할때 한글 깨지는 문제를 해결
    - Window는 Malgun Gothic으로 Mac은 AppleGothic으로 설정함
    - 코드를 실행하는 과정에서 에러가 발생할 경우 except로 빠져 아무일도 하지 않음

--- pykrx/__init__.py
+++ pykrx/__init__.py
@@ -1,1 +1,16 @@
 from pykrx.version import __version__
+
+try:
+    import platform
+    import matplotlib.pyplot as plt
+    import matplotlib.font_manager as fm
+
+    os = platform.system()
+    if os == "Darwin":
+        # fonts = [x.name for x in fm.fontManager.ttflist if 'AppleGothic' in x.name]
+        plt.rc('font', family="AppleGothic")
+    else:
+        # fonts = [x.name for x in fm.fontManager.ttflist if 'Malgun Gothic' in x.name]
+        plt.rc('font', family="Malgun Gothic")
+except:
+    pass
