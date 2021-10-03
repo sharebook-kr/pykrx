107101d62290413e463c4338ef04e29faeeb7039
jonghun.yoo <jonghun.yoo@outlook.com>
2021-10-03 20:22:04 +0900

    Naver 스크래핑에서 상폐종목의 에러처리

--- pykrx/website/naver/wrap.py
+++ pykrx/website/naver/wrap.py
@@ -14,19 +14,23 @@ def get_market_ohlcv_by_date(fromdate, todate, ticker):
     xml = Sise().fetch(ticker, elapsed.days)
 
     result = []
-    for node in et.fromstring(xml).iter(tag='item'):
-        row = node.get('data')
-        result.append(row.split("|"))
+    try:
+        for node in et.fromstring(xml).iter(tag='item'):
+            row = node.get('data')
+            result.append(row.split("|"))
 
-    cols = ['날짜', '시가', '고가', '저가', '종가', '거래량']
-    df = DataFrame(result, columns=cols)
-    df = df.set_index('날짜')
-    df.index = pd.to_datetime(df.index, format='%Y%m%d')
-    df = df.astype(np.int32)
-    return df.loc[(strtd <= df.index) & (df.index <= lastd)]
+        cols = ['날짜', '시가', '고가', '저가', '종가', '거래량']
+        df = DataFrame(result, columns=cols)
+        df = df.set_index('날짜')
+        df.index = pd.to_datetime(df.index, format='%Y%m%d')
+        df = df.astype(np.int32)
+        return df.loc[(strtd <= df.index) & (df.index <= lastd)]
+    except et.ParseError:
+        return None
 
 
 if __name__ == "__main__":
     # df = get_market_ohlcv_by_date("20010101", "20190820", "005930")
-    df = get_market_ohlcv_by_date("20200226", "20200227", "000020")
+    # df = get_market_ohlcv_by_date("20200226", "20200227", "000020")
+    df = get_market_ohlcv_by_date("19991220", "20191231", "008480")
     print(df)
