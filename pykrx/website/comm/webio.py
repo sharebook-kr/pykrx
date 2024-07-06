6cdb3b137f0dffcdf255668106a586b3220bbabe
Seunghoon Shin <liante0904@gmail.com>
2024-07-06 23:33:46 +0900

    Bugfix webio. Add Referer at response header

--- pykrx/website/comm/webio.py
+++ pykrx/website/comm/webio.py
@@ -4,7 +4,7 @@ from abc import abstractmethod
 
 class Get:
     def __init__(self):
-        self.headers = {"User-Agent": "Mozilla/5.0"}
+        self.headers = {"User-Agent": "Mozilla/5.0", "Referer": "http://data.krx.co.kr/"}
 
     def read(self, **params):
         resp = requests.get(self.url, headers=self.headers, params=params)
@@ -18,7 +18,7 @@ class Get:
 
 class Post:
     def __init__(self, headers=None):
-        self.headers = {"User-Agent": "Mozilla/5.0"}
+        self.headers = {"User-Agent": "Mozilla/5.0", "Referer": "http://data.krx.co.kr/"}
         if headers is not None:
             self.headers.update(headers)
 
