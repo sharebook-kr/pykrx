df871e5d07c0988b9590367bb400e0b511b0b67e
jonghun.yoo <jonghun.yoo@outlook.com>
2025-05-25 00:58:44 +0900

    chore: update setup.py for PyPI packaging

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 from setuptools import setup, find_packages
 from pykrx import __version__
 
-# main_ns = {}
-# ver_path = convert_path('pykrx/version.py')
-# with open(ver_path) as ver_file:
-#     exec(ver_file.read(), main_ns)
-
 with open("README.md", "r", encoding='UTF-8') as fh:
     long_description = fh.read()
 
@@ -13,7 +8,7 @@ setup(
     name='pykrx',
     version=__version__,
     description='KRX data scraping',
-    url='https://github.com/liante0904/pykrx',
+    url='https://github.com/sharebook-kr/pykrx/',
     author='Brayden Jo, Jonghun Yoo',
     author_email=('brayden.jo@outlook.com, '
                   'jonghun.yoo@outlook.com, '
